9ad3e52fd788a60778dfa9fc229bcf4e3fe44d2a
phantom9999 <1057369361@qq.com>
2020-11-10 23:10:42 +0800

    add glog

--- build.sh
+++ build.sh
@@ -3,11 +3,22 @@ set -e
 
 workspace=`pwd`;
 
-wget https://github.com/gflags/gflags/archive/v2.2.2.tar.gz -O gflags-2.2.2.tar.gz
-tar -xf gflags-2.2.2.tar.gz
-cd gflags-2.2.2/
+# 拉取依赖
+mkdir -p deps;
+cd deps;
+wget https://github.com/phantom9999/bazel_rule/releases/download/gflags-2.2.2/output.zip
+unzip output.zip
+
+cd ${workspace};
+export CMAKE_INCLUDE_PATH=${workspace}/deps/output/include;
+export CMAKE_LIBRARY_PATH=${workspace}/deps/output/lib:${workspace}/deps/output/lib64;
+
+# 下载源码
+wget https://github.com/google/glog/archive/v0.4.0.tar.gz \
+  -O glog-0.4.0.tar.gz
+tar -xf glog-0.4.0.tar.gz
+cd glog-0.4.0/
 cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DCMAKE_INSTALL_PREFIX=${workspace}/output \
-    -DGFLAGS_NAMESPACE=google
+    -DCMAKE_INSTALL_PREFIX=${workspace}/output 
 make install -j4
 
