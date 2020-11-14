7ad8412b0c947e704d44f8e2e8950d566d06509b
phantom9999 <1057369361@qq.com>
2020-11-14 17:23:49 +0800

    fix

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 workspace=`pwd`;
 
 wget https://github.com/google/snappy/archive/1.1.8.tar.gz \
-    snappy-1.1.8.tar.gz
+    -O snappy-1.1.8.tar.gz
 tar -xf snappy-1.1.8.tar.gz;
 cd snappy-1.1.8;
 cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
