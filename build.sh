9a848a0bcc53a512d1e07a9524e0486b8dbac8b3
phantom9999 <1057369361@qq.com>
2020-11-15 11:22:50 +0800

    add centos7

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ workspace=`pwd`;
 wget https://github.com/gflags/gflags/archive/v2.2.2.tar.gz -O gflags-2.2.2.tar.gz
 tar -xf gflags-2.2.2.tar.gz
 cd gflags-2.2.2/
-cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+cmake3 . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_INSTALL_PREFIX=${workspace}/output \
     -DGFLAGS_NAMESPACE=google
 make install -j4
