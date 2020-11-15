87160e180053dd72d5499d7406ceb4d4465e26ea
phantom9999 <1057369361@qq.com>
2020-11-15 17:37:03 +0800

    add tar.gz

--- build.sh
+++ build.sh
@@ -9,9 +9,10 @@ wget https://github.com/google/leveldb/archive/1.22.tar.gz -O leveldb-1.22.tar.g
 tar -xf leveldb-1.22.tar.gz;
 cd leveldb-1.22;
 
-cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+cmake3 . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_INSTALL_PREFIX=${workspace}/output \
     -DCMAKE_CXX_FLAGS=-fPIC \
+    -DCMAKE_INSTALL_LIBDIR=lib \
     -DLEVELDB_BUILD_TESTS=OFF \
     -DLEVELDB_BUILD_BENCHMARKS=OFF
 make install -j4
