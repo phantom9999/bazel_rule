319370564b287ad5ee0a78442f07b26c8452aa13
phantom9999 <1057369361@qq.com>
2020-11-22 17:34:52 +0800

    add build

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ echo ${OS_ENV};
 workspace=`pwd`;
 
 wget https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.gz -O boost_1_74_0.tar.gz
-tar -xvf boost_1_74_0.tar.gz
+tar -xf boost_1_74_0.tar.gz
 cd boost_1_74_0/;
 
 sh bootstrap.sh
-./b2 --help
+./b2 variant=release link=static threading=multi runtime-link=shared --prefix=${workspace}/output -j4 install
 
