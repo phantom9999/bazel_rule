46fc436b1c66fb0363f40505b8b3ce9c00044fb4
phantom9999 <1057369361@qq.com>
2020-11-22 20:59:20 +0800

    add fmt

--- build.sh
+++ build.sh
@@ -5,11 +5,10 @@ echo ${OS_ENV};
 
 workspace=`pwd`;
 
-wget https://github.com/gflags/gflags/archive/v2.2.2.tar.gz -O gflags-2.2.2.tar.gz
-tar -xf gflags-2.2.2.tar.gz
-cd gflags-2.2.2/
+wget https://github.com/fmtlib/fmt/archive/7.1.2.tar.gz -O fmt-7.1.2.tar.gz
+tar -xf fmt-7.1.2.tar.gz
+cd fmt-7.1.2/
 cmake3 . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DCMAKE_INSTALL_PREFIX=${workspace}/output \
-    -DGFLAGS_NAMESPACE=google
+    -DCMAKE_INSTALL_PREFIX=${workspace}/output
 make install -j4
 
