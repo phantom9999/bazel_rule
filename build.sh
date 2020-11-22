d9fc5efdb2cf4028428ae90e42330a4de7f1b5ee
phantom9999 <1057369361@qq.com>
2020-11-22 20:40:50 +0800

    fix

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ wget https://github.com/abseil/abseil-cpp/archive/20200923.2.tar.gz -O abseil-cp
 tar -xf abseil-cpp-20200923.2.tar.gz
 cd abseil-cpp-20200923.2/
 cmake3 . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DCMAKE_INSTALL_PREFIX=${workspace}/output
+    -DCMAKE_INSTALL_PREFIX=${workspace}/output \
+    -DCMAKE_CXX_STANDARD=14 \
+    -DCMAKE_CXX_STANDARD_REQUIRED=ON \
+    -DCMAKE_CXX_EXTENSIONS=OFF
 make install -j4
 
