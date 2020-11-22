9f1115e6ffb9f05572ac5a29ec419b6062a93a8e
phantom9999 <1057369361@qq.com>
2020-11-22 22:06:55 +0800

    fix

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ cd Catch2-2.13.3/
 mkdir build/;
 cd build;
 cmake3 .. -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DCMAKE_INSTALL_PREFIX=${workspace}/output
+    -DCMAKE_INSTALL_PREFIX=${workspace}/output \
+    -DCATCH_BUILD_TESTING=OFF
 make install -j4
 
