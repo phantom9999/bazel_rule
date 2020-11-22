8fe6e3f75706aff36acf34e64b3cb311b9461043
phantom9999 <1057369361@qq.com>
2020-11-22 23:41:40 +0800

    rm shared

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ wget https://github.com/libevent/libevent/archive/release-2.1.12-stable.tar.gz -
 tar -xf libevent-release-2.1.12-stable.tar.gz
 cd libevent-release-2.1.12-stable/
 cmake3 . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DCMAKE_INSTALL_PREFIX=${workspace}/output
+    -DCMAKE_INSTALL_PREFIX=${workspace}/output \
+    -DEVENT__LIBRARY_TYPE=STATIC
+
 make install -j4
 
